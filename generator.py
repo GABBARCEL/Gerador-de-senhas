1748523db949beaa0c0ea30601e01cad59fa4770
Gabriel Barcelos <gabriel.barcelos@tutamail.com>
2026-01-08 00:49:56 -0300

    Alterei os nomes das variaveis e funções, em breve haverá função de senha com filtro!

--- generator.py
+++ generator.py
@@ -1,39 +1,40 @@
 from random import choice, shuffle
 
-letras = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
-numeros = ["1","2","3","4","5","6","7","8","9","0"]
-caracteres_especiais = ["!","@","#","$","%","&","*","_","+"]
-
-def gerar_sequencia(Size_letter = 4, Size_numbers = 3, Size_special = 1, letter_mode = "mx"):
+def new_password(size_letter = 4, size_numbers = 3, size_special = 1, letter_mode = "mx"):
+    letras = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+    numeros = ["1","2","3","4","5","6","7","8","9","0"]
+    caracteres_especiais = ["!","@","#","$","%","&","*","_","+"]
+    
+    ### ↑↑↑↑↑ CONSIDERANDO OS CARACTERES, CASO QUEIRA INCLUIR OU RETIRAR DE POSSIBILIDADE, MECHA AQUI !!! ↑↑↑↑↑
     gen_completo = []
-    for l in range(Size_letter): # PARA GERAR AS LETRAS
+    for l in range(size_letter): # PARA GERAR AS LETRAS
         gen_letra = choice(letras)
         if letter_mode == "mx": # MODO MIXER
-            
+                    
             if choice([True, False]):
                 maiscula = gen_letra.upper()
                 gen_completo.append(maiscula)
             else:
                 gen_completo.append(gen_letra)
-        
+                
         elif letter_mode == "+": # MODO MAIÚSCULA
             maiscula = gen_letra.upper()
             gen_completo.append(maiscula)
-        
+                
         elif letter_mode == "-": # MODO MINÚSCULA
             gen_completo.append(gen_letra)
 
 
-    for n in range(Size_numbers): # PARA GERAR OS NÚMEROS
+    for n in range(size_numbers): # PARA GERAR OS NÚMEROS
         gen_numero = choice(numeros)
         gen_completo.append(gen_numero)
 
 
-    for c in range(Size_special): # PARA GERAR OS CARACTERES ESPECIAIS
+    for c in range(size_special): # PARA GERAR OS CARACTERES ESPECIAIS
         gen_caracter = choice(caracteres_especiais)
         gen_completo.append(gen_caracter)
 
     shuffle(gen_completo)
     return "".join(gen_completo)
     
-print(gerar_sequencia(5,3,1,"mx")) 
+print(new_password(5,3,1,"mx"))
